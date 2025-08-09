2025ef2eb19354d8d31ac9c8830ca2d7c2bbe25c
최윤진 <opficdev@gmail.com>
2025-08-09 13:25:13 +0900

    feat: 웹뷰 올려볼 스토리보드 추가

---
+++ MyWebBrowser/MyWebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWebBrowser
+//
+//  Created by opfic on 8/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
