1ef55b94bd617e19e5716a04182cb4aaa7a0ab57
최윤진 <opficdev@gmail.com>
2025-08-11 16:06:06 +0900

    feat: 인터페이스 빌더로 레이아웃 구성하기

---
+++ MyProfile/MyProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProfile
+//
+//  Created by opfic on 8/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
