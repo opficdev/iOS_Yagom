53859a79919d8ed881107a36b78e066f5e782c42
opfic <opficdev@gmail.com>
2025-08-12 10:47:54 +0900

    feat: Initial project setup for UpDown iOS app
    Add Xcode project files, basic app structure, assets, storyboards, and initial Swift source files for AppDelegate, SceneDelegate, and ViewController. This sets up the foundation for the UpDown iOS application.

---
+++ UpDown/UpDown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDown
+//
+//  Created by opfic on 8/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
