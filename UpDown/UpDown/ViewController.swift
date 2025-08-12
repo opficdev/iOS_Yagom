3980fcfdf0d3ef7326162537fda54611937ce853
opfic <opficdev@gmail.com>
2025-08-12 16:46:10 +0900

    feat: Add actions for submit and reset buttons
    Connected the submit and reset buttons in Main.storyboard to new IBAction methods in ViewController.swift. These methods currently log button taps and set up the structure for future functionality.

--- UpDown/UpDown/ViewController.swift
+++ UpDown/UpDown/ViewController.swift
@@ -13,12 +13,21 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var resultLabel: UILabel!
     @IBOutlet private weak var turnCountLabel: UILabel!
     @IBOutlet private weak var inputField: UITextField!
+    
+    // MARK: - Methods
+    // 사용자가 제출 버튼을 탭하는 액션
+    @IBAction func touchUpSubmitButton(_ sender: UIButton) {
+        print("touch up submit button")
+    }
 
+    // 사용자가 리셋 버튼을 탭하는 액션
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    
 }
 
