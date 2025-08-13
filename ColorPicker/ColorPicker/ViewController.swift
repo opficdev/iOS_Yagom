ee0a197f1afeea85344fb6700c82f0d5f5f9a1e3
opfic <opficdev@gmail.com>
2025-08-13 09:58:39 +0900

    feat: Connect UI outlets and slider actions in ViewController
    Added IBOutlet connections for colorView and pickerView, and hooked up slider valueChanged actions in the storyboard. Introduced a stub for sliderValueChanged IBAction in ViewController.swift to handle slider events.

--- ColorPicker/ColorPicker/ViewController.swift
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
