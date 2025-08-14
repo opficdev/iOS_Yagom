77b73faf1af4c3be06ddf899cf90fd9034e4d4e5
opfic <opficdev@gmail.com>
2025-08-14 17:52:45 +0900

    feat: Initial Commit

---
+++ CompanionAnimals/CompanionAnimals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanionAnimals
+//
+//  Created by opfic on 8/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
