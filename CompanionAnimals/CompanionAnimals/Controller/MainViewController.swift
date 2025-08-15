1769bdaff4ddb838bb45bf1b282ef5fd9ab3f6bf
opfic <opficdev@gmail.com>
2025-08-15 22:38:03 +0900

    feat: 프로젝트 최초 설정

---
+++ CompanionAnimals/CompanionAnimals/Controller/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  CompanionAnimals
+//
+//  Created by opfic on 8/14/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
